ef7c56a0bda39882f12bf62a6094f017b40ca92f
Clement Ruhm <clement@balacoon.com>
2022-10-23 11:31:51 +0200

    speechbrain/dataio/dataio.py: fix tracing warning

--- speechbrain/dataio/dataio.py
+++ speechbrain/dataio/dataio.py
@@ -694,7 +694,7 @@ def length_to_mask(length, max_len=None, dtype=None, device=None):
         max_len = length.max().long().item()  # using arange to generate mask
     mask = torch.arange(
         max_len, device=length.device, dtype=length.dtype
-    ).expand(len(length), max_len) < length.unsqueeze(1)
+    ).expand(length.shape[0], max_len) < length.unsqueeze(1)
 
     if dtype is None:
         dtype = length.dtype
@@ -702,7 +702,7 @@ def length_to_mask(length, max_len=None, dtype=None, device=None):
     if device is None:
         device = length.device
 
-    mask = torch.as_tensor(mask, dtype=dtype, device=device)
+    #mask = torch.as_tensor(mask, dtype=dtype, device=device)
     return mask
 
 
